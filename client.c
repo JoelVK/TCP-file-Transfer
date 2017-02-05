1833282db2a4abcbd74e4b5ac5ae392c65b94672
Brecker <breckerm@eos11.cis.gvsu.edu>
2017-02-05 15:25:48 -0500

    Fixed a couple bugs and cleaned up output

--- client.c
+++ client.c
@@ -15,102 +15,123 @@
 #include <arpa/inet.h>
 #include <libgen.h>
 #include <math.h>
+
+int getportnum();
+void getip(char* dst, int size);
+
 int main (int argc, char** argv) {
-	int sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd<0){
-	    printf("There was an error creating a socket\n");
-		return 1;
+  int sockfd = socket(AF_INET,SOCK_STREAM,0);
+  if(sockfd<0){
+    printf("There was an error creating a socket\n");
+    return 1;
+  }
+
+  //Get IP and Port number
+  char ipaddr[13];
+  int portnum = getportnum();
+  getip(ipaddr, sizeof(ipaddr));
+  
+  //Connect to server
+  struct sockaddr_in serveraddr;
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_port=htons(portnum);
+  serveraddr.sin_addr.s_addr=inet_addr(ipaddr);
+  int e = connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+  if(e<0){
+    printf("There was an error connecting to port %d and IP %s\n", portnum, ipaddr);
+    return 1;
+  }
+  
+  char menu[4096];
+  char fp[5000];
+  while(1){
+    printf("\n-------------------------------\n");
+    recv(sockfd, menu, sizeof(menu),0);
+    printf(menu);
+    printf("\n");
+    
+    //Get user's choice
+    printf("Enter choice: ");
+    fgets(fp,5000,stdin);
+    fp[strlen(fp)-1] = '\0';  //Remove newline character
+    send(sockfd,fp,strlen(fp)+1,0);
+    
+    if(strncmp(fp, "0", 1)==0){
+      //chose to exit
+      break;
+    } else if(strncmp(fp, "1", 1) == 0){
+      //print list of files
+      char filelist[12288];
+      recv(sockfd,filelist, sizeof(filelist), 0);
+      printf("\nAvailable Files: \n");
+      printf("%s\n", filelist);
+      continue;
+    } else {
+      //Server responds with either a Y or N to signal
+      //if it could find the file
+      char result[1];
+      recv(sockfd,result, sizeof(result),0);
+      if(strncmp(result, "N",1)==0){
+	printf("Server could not find file\n");
+	continue;
+      }
+      
+      //Create file for destination of transfer
+      char* filename = basename(fp);
+      FILE* file = fopen(filename, "w");
+      if(file == NULL){
+	printf("Error: Failed to open/create file\n");
+	continue;
+      }
+      
+      //check number of expected packets
+      int size, numPackets;
+      uint32_t sendSize;
+      char dataBuf[128];
+      int numBytesRead = 0;
+      
+      recv(sockfd,&sendSize,sizeof(sendSize),0);
+      size = ntohl(sendSize);
+      numPackets = ceil((double)size / sizeof(dataBuf));
+      //transfer the file
+      do {
+	numBytesRead = recv(sockfd,dataBuf,sizeof(dataBuf),0);
+	if(numBytesRead < 0){
+	  printf("Error: failed to read data.\n");
+	} else if(numBytesRead > 0) {
+	  fwrite(dataBuf, 1, numBytesRead, file);
 	}
-
-	//Get IP and Port number
-	char portnum[6], ipaddr[20];
-	printf("Enter the port number: \n");
-	fgets(portnum,6,stdin);
-	int num = atoi(portnum);
-	if(num < 1024 || num > 49151){
-	    printf("Error: Invalid port number.\n");
-	    return 1;
-	}
-	printf("Enter the IP: \n");
-	fgets(ipaddr,20,stdin);
-	
-	//Connect to server
-	struct sockaddr_in serveraddr;
-	serveraddr.sin_family=AF_INET;
-	serveraddr.sin_port=htons(atoi(portnum));
-	serveraddr.sin_addr.s_addr=inet_addr(ipaddr);
-	int e = connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
-	if(e<0){
-	    printf("There was an error connecting \n");
-		return 1;
-	}
-	
-	char menu[4096];
-	char fp[5000];
-	while(1){
-	
-	    recv(sockfd, menu, sizeof(menu),0);
-	    printf(menu);
-	    printf("\n");
-
-	    //Get user's choice
-	    printf("Enter choice: ");
-	    fgets(fp,5000,stdin);
-	    fp[strlen(fp)-1] = '\0';  //Remove newline character
-	    send(sockfd,fp,strlen(fp)+1,0);
-	  
-	    if(strncmp(fp, "0", 1)==0){
-	        //chose to exit
-	        break;
-	    } else if(strncmp(fp, "1", 1) == 0){
-	        //print list of files
-	        char filelist[12288];
-	        recv(sockfd,filelist, sizeof(filelist), 0);
-	        printf("\n");
-	        continue;
-	    } else {
-	        //Server responds with either a Y or N to signal
-	        //if it could find the file
-	        char result[1];
-	        recv(sockfd,result, sizeof(result),0);
-	        if(strncmp(result, "N",1)==0){
-	            printf("Server could not find file\n");
-	            continue;
-	        }
-
-	        //Create file for destination of transfer
-	        char* filename = basename(fp);
-	        FILE* file = fopen(filename, "w");
-	        if(file == NULL){
-		        printf("Error: Failed to open/create file\n");
-		        continue;
-	        }
-
-            //check number of expected packets
-            int size, numPackets;
-            uint32_t sendSize;
-            char dataBuf[128];
-            int numBytesRead = 0;
-           
-            recv(sockfd,&sendSize,sizeof(sendSize),0);
-            size = ntohl(sendSize);
-            numPackets = ceil((double)size / sizeof(dataBuf));
-            //transfer the file
-	        do {
-		        numBytesRead = recv(sockfd,dataBuf,sizeof(dataBuf),0);
-		        if(numBytesRead < 0){
-		            printf("Error: failed to read data.\n");
-		        } else if(numBytesRead > 0) {
-		            fwrite(dataBuf, 1, numBytesRead, file);
-     	        }
-                numPackets--;
-	        }
-	        while(numBytesRead > 0 && numPackets > 0);
-	        fclose(file);
-	        send(sockfd,"Done",5,0);
-	    }
+	numPackets--;
+      }
+      while(numBytesRead > 0 && numPackets > 0);
+      fclose(file);
+      send(sockfd,"Done",5,0);
+      printf("File transfer complete\n\n");
     }
-	
-    return 0;
+
+  }
+  
+  return 0;
 }
 
+int getportnum()
+{
+  int num = 0;
+  while(1) {
+    printf("Enter the port number: ");
+    scanf("%d", &num);
+    while(getchar() != '\n');  //Clear stdin
+    if(num >= 1024 && num <= 49151) break;  //Check for valid input
+    printf("Invalid port number, try again.\n");
+  }
+  return num;
+}
+
+void getip(char* dst, int size)
+{
+  printf("Enter IP address: ");
+  fgets(dst, size, stdin);
+  if(dst[strlen(dst) - 1] == '\n') {
+    dst[strlen(dst) - 1] = '\0';
+  }
+}
--- serverside.c
+++ serverside.c
@@ -17,123 +17,111 @@
 #include <dirent.h>
 
 int getPortNum();
+int clientcount = 0;
 
 void* clienthandler(void* arg){
-    int clientsocket = *(int*) arg;
+  int clientsocket = *(int*) arg;
   
-    char msg[4096];
-    char menu[] = "0: Exit \n1: List Available Files \nOtherwise Enter File\n";
-    while(1){
-        //Send choices/menu to client (list of available files, exit)
-        send(clientsocket, menu, strlen(menu)+1, 0); 
-
-        //Receive choice from client
-        recv(clientsocket,msg,sizeof(msg),0);
-        printf("Message: %s\n", msg);
-
-        if(strncmp(msg, "0", 1)==0){
-            printf("Client chose to exit\n");
-            //exit
-            break;
+  char msg[4096];
+  char menu[] = "0: Exit \n1: List Available Files \nOtherwise Enter File\n";
+  while(1){
+    //Send choices/menu to client (list of available files, exit)
+    send(clientsocket, menu, strlen(menu)+1, 0); 
+    
+    //Receive choice from client
+    recv(clientsocket,msg,sizeof(msg),0);
+    
+    if(strncmp(msg, "0", 1)==0){
+      //exit
+      break;
+      
+    } else if(strncmp(msg, "1", 1)==0){
+      //print out list of files
+      DIR* d;
+      struct dirent* dir;
+      
+      if((d = opendir(".")) != NULL){
+	char filelist[12288];
+	filelist[0] = '\0'; //reset
+	while((dir = readdir(d)) != NULL){
+	  sprintf(filelist + strlen(filelist), "%s\n", dir->d_name);
+	}
+	send(clientsocket, filelist, sizeof(filelist), 0);
+	closedir(d);
+      }
+    } else{
+      //Check if file exists, send message Y or N
+      FILE* file = fopen(msg, "r");
+      
+      if(file == NULL){		  
+	send(clientsocket,"N", 1, 0); 
+      } else {
+	
+	send(clientsocket,"Y", 1, 0); 
+	char dataBuf[128];
+	int numBytesRead = 0;
+	int size;
+	uint32_t sendSize;
         
-        } else if(strncmp(msg, "1", 1)==0){
-            //print out list of files
-            printf("Client chose to view available files\n");
-            DIR* d;
-            struct dirent* dir;
-        
-            if((d = opendir(".")) != NULL){
-	            char filelist[12288];
-	            while((dir = readdir(d)) != NULL){
-	                sprintf(filelist + strlen(filelist), "%s\n", dir->d_name);
-	            }       
-	            send(clientsocket, filelist, sizeof(filelist), 0);
-	            closedir(d);
-            }
-        } else{
-	        //Check if file exists, send message Y or N
-	        FILE* file = fopen(msg, "r");
-	    
-            if(file == NULL){		  
-	            send(clientsocket,"N", 1, 0); 
-	        } else {
-
-	            send(clientsocket,"Y", 1, 0); 
-	            char dataBuf[128];
-	            int numBytesRead = 0;
-                int size;
-                uint32_t sendSize;
-                
-                //get size of file in bytes
-                fseek(file,0,SEEK_END);
-                size = ftell(file);
-                fseek(file,0,SEEK_SET);
-	            
-                sendSize = htonl(size);
-                send(clientsocket,&sendSize,sizeof(sendSize),0);
-                while((numBytesRead = fread(dataBuf, 1, sizeof(dataBuf),file)) > 0){
-	                printf("Sending packet with %d bytes\n", numBytesRead);
-	                send(clientsocket,dataBuf,numBytesRead,0);
-	            }
-	            recv(clientsocket,msg,sizeof(msg), 0); 
-	            fclose(file);
-	        }
-        }        
-    }
+	//get size of file in bytes
+	fseek(file,0,SEEK_END);
+	size = ftell(file);
+	fseek(file,0,SEEK_SET);
+	
+	sendSize = htonl(size);
+	send(clientsocket,&sendSize,sizeof(sendSize),0);
+	while((numBytesRead = fread(dataBuf, 1, sizeof(dataBuf),file)) > 0){
+	  send(clientsocket,dataBuf,numBytesRead,0);
+	}
+	recv(clientsocket,msg,sizeof(msg), 0); 
+	fclose(file);
+      }
+    }        
+  }
   
-    close(clientsocket);
-    printf("The client has disconnected\n");
-    //exit(0);
-    return 0;
+  close(clientsocket);
+  clientcount--;
+  printf("Client has disconnected. %d client(s) still connected.\n", clientcount);
+
+  return 0;
 }
 
 int main( int argc, char **argv){	
-	int portnum  = getPortNum();
-	
-	int sockfd = socket(AF_INET,SOCK_STREAM,0);
-
-	struct sockaddr_in serveraddr, clientaddr;
-	serveraddr.sin_family=AF_INET;
-	serveraddr.sin_port=htons(portnum);
-	serveraddr.sin_addr.s_addr=INADDR_ANY;
-
-	bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
-	listen(sockfd,10);
-
-	//printf("Press 0 to exit\n");
-	//char exitBuf[5];
-	while(1) {
-		socklen_t len = sizeof(clientaddr);
-		int clientsocket = accept(sockfd,(struct sockaddr*)&clientaddr,&len);
-		printf("Client has connected\n");
-
-		//Create child process to handle client
-		pthread_t child;
-		pthread_create(&child, NULL, clienthandler, &clientsocket);
-		pthread_detach(child);
-		
-		/*
-		int numBytes = fgets(exitBuf,sizeof(exitBuf),stdin);  
-		if(numByes > 0 && strncmp(exitBuf, "0", 1)==0) {
-		  //close threads
-		  pthread_cancel(child);
-		  break;
-		}
-		*/
-		
-	}
-	return 0;	
+  int portnum  = getPortNum();
+  
+  int sockfd = socket(AF_INET,SOCK_STREAM,0);
+  
+  struct sockaddr_in serveraddr, clientaddr;
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_port=htons(portnum);
+  serveraddr.sin_addr.s_addr=INADDR_ANY;
+  
+  bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+  listen(sockfd,10);
+  
+  printf("\nWaiting for clients...\n");
+  while(1) {
+    socklen_t len = sizeof(clientaddr);
+    int clientsocket = accept(sockfd,(struct sockaddr*)&clientaddr,&len);
+    clientcount++;
+    printf("Client has connected. %d client(s) now connected\n", clientcount);
+    
+    //Create child process to handle client
+    pthread_t child;
+    pthread_create(&child, NULL, clienthandler, &clientsocket);
+    pthread_detach(child);
+  }
+  return 0;	
 }
 
 int getPortNum(){
-    int portnum  = 0;
-    while(portnum < 1024 || portnum > 49151){
-        printf("Enter the port number: ");
-        scanf("%d", &portnum);
-        if(portnum < 1024 || portnum > 49151){
-	        printf("Invalid port number\n");
-        }
-        getchar();//clear stdin
-    }
-    return portnum;
+  int portnum  = 0;
+  while(1){
+    printf("Enter the port number: ");
+    scanf("%d", &portnum);
+    while(getchar() != '\n');  //Clear stdin
+    if(portnum >= 1024 && portnum <= 49151) break; //Check for valid input
+    printf("Invalid port number, try again.\n");
+  }
+  return portnum;
 }
